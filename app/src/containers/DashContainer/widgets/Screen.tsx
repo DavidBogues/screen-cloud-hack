856d36487ebc1648b4703747a7f67aabec3cd370
DavidBogues <david.bogues@screencloud.io>
2019-11-05 16:52:28 +0700

    stats png fixed and animating

--- app/src/containers/DashContainer/widgets/Screen.tsx
+++ app/src/containers/DashContainer/widgets/Screen.tsx
@@ -1,9 +1,10 @@
-import React, { Component, ReactNode, Fragment } from "react";
+import React, { Component, ReactNode, Fragment, ErrorInfo } from "react";
 
 import { IScreen } from "../../../models/IScreen";
 
 interface State {
-    screen:IScreen
+    screen:IScreen,
+    currentImage: string
 }
 
 interface IProps {
@@ -21,11 +22,39 @@ export class Screen extends Component<IProps, State> {
     constructor(props: IProps) {
         super(props);
 
-        props.screen.currentImage=props.screen.playing[0];
+        
         this.state = {
             screen:props.screen,
-
+            currentImage:props.screen.playing[0]
         };
+
+    }
+
+    updateScreen = () => {
+        var index = this.state.screen.playing.indexOf(this.state.currentImage);
+        console.log(index);
+        index+=1;
+        if(index >= this.state.screen.playing.length){
+            index = 0;
+        }
+        var newImage = this.state.screen.playing[index];
+        this.setState({
+            currentImage: newImage
+        });
+
+        setTimeout(() => {
+            this.updateScreen();
+        }, 3000);
+
+    }
+
+    componentDidMount(): void {
+        
+        
+        setTimeout(() => {
+            this.updateScreen();
+        }, 3000);
+        
     }
 
 
@@ -40,7 +69,7 @@ export class Screen extends Component<IProps, State> {
 
                     <div className={this.state.screen.status === "online" ? "screen" : "screen offline"}>
                         {this.state.screen.status === "online"  &&
-                            <img src={`./img/${this.state.screen.currentImage}`} />
+                            <img src={`./img/${this.state.currentImage}`} />
                         }
                         <div className="screen-name">
                             {this.state.screen.name}
